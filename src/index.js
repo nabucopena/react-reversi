cb095b14ae927bb2ba13e16f855b88d830462a82
Felix N. Peña <nabucopena@gmail.com>
2022-03-08 22:35:38 -0300

    Reversi initial commit
    At this point, there is a board where two players can place tiles by turn.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+function basicBoard() {
+  const board = new Array(8*8).fill('•');
+  board[27] = 'O';
+  board[36] = 'O';
+  board[28] = 'X';
+  board[35] = 'X';
+  return (board)
+}
+
+
+function Square(props) {
+
+  return (
+    <button
+      className="square"
+      onClick={
+        props.value=='•'? props.onClick : null
+      }
+    >
+      {props.value}
+    </button>
+  );
+}
+
+function Board(props) {
+  const range = new Array(props.boardWidth).fill(0);
+
+  const board =
+    range.map( (_, i) =>
+      <div key={i} className="board-row">
+        { range.map( (_,j) => <Square
+          key={ j+i*props.boardWidth }
+          value={props.squares[ j+i*props.boardWidth ]}
+          onClick={() => props.onClick( j+i*props.boardWidth )}
+        /> ) }
+      </div>
+      );
+  
+  return (board)
+}
+
+function Game(props) {
+  const boardWidth = 8
+  const [turn, setTurn] = useState('X');
+  
+  function handleClick(square) {
+    const newBoard = [...squares]
+    newBoard[square] = turn
+    setTurn( (turn=='X'? 'O' : 'X') );
+    setSquares( newBoard );
+  }
+
+  const [squares, setSquares] = useState(basicBoard())
+
+
+  return (
+    <div>
+      <Board
+        squares={squares}
+        onClick={(i) => {handleClick(i)}}
+        boardWidth={boardWidth}
+      />
+    </div>
+  )
+}
+
+
+
+// ================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
